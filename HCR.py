6d31ee633e86dd4253ad1174aff6fb321b1999bb
gxcfr <gajendrac@outlook.com>
2026-06-18 23:13:36 +0530

    Camera Setup

--- HCR.py
+++ HCR.py
@@ -1,3 +1,11 @@
 import cv2
 
-cap= cv2.VideoCapture(1)
+cap= cv2.VideoCapture(0)
+cap.set(3, 600)
+cap.set(4,400)
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img,1)
+    cv2.imshow('Camera Feed', img)
+    cv2.waitKey(1)
